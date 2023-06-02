6f34cb2ed63369c5707c4506907db8c7745ed55f
mujahidfisher <fishermujahid@gmail.com>
2023-06-02 16:37:16 +0200

    cant anymore

--- script/code.js
+++ script/code.js
@@ -1,19 +1,20 @@
 let sortItem = document.querySelector("#sortItems")
-
+let line = document.querySelector("#check")
 let addItem = document.querySelector("#addItems")
+let arr = [""]
 
 addItem.addEventListener("click", (e)=> {
     e.preventDefault()
     let inputItem = document.querySelector("#inputItems").value
     let output = document.querySelector("#outputItems")
-    output.innerHTML += inputItem 
-    let arr = []
-    input = arr
-    
+    output.innerHTML += `<p class="stuff" id="outputItems">${inputItem}
+    </p>`
+    arr = inputItem
+    console.log(arr);
+    inputItem = ''
+    let removeOne = document.querySelector("#removeItem")
+    removeOne = pop(output)
 })
-
-
-
-
-
-
+if (line.checked){
+        let output = output.strike()
+    }
